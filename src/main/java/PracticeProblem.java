bdc3f9ac99e70eeb038f5ff23cde4c700650a705
usain-boltsMM <343421046@tdsb.ca>
2026-03-23 11:21:39 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 class PracticeProblem {
 
-	static Scanner a = new Scanner(System.in);
+	
   	public static void main(String[] args) {
 		
 		q1();
@@ -18,6 +18,7 @@ class PracticeProblem {
 		q4();
   	}
   	public static void q1() {
+		static Scanner a = new Scanner(System.in);
 		// From input, recieve two integers from the user and add them together.  Output the result.
 		int num1;
 		System.out.print("Input a number: ");
@@ -31,6 +32,7 @@ class PracticeProblem {
   	}
 
 	public static void q2() {
+		static Scanner a = new Scanner(System.in);
 		// From input recieve two integers.  Output the quotient rounded down.
 		int num1;
 		System.out.print("Input a number: ");
@@ -44,12 +46,14 @@ class PracticeProblem {
 	}
 
 	public static void q3() {
+		static Scanner a = new Scanner(System.in);
 		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?"
 		
 		System.out.println("hello \"Mr. Kalisz\" have you seen my work yet?");
 	}
 
 	public static void q4() {
+		static Scanner a = new Scanner(System.in);
 		// From input recieve two numbers (can be decimal fractions).  
 		// Output the first number plus two, then multiplied by the second number.
 		Double num1;
